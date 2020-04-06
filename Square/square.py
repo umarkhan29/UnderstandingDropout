6f6f720db529f1036179b2791ec4e0ae3e002acd
Umar Khan <umee909@gmail.com>
2020-04-06 16:30:32 +0530

    Create square.py

---
+++ Square/square.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Dropout
+from keras.optimizers import Adam
+import numpy as np
+
+print("TF Version")
+print(tf.__version__)
+
+
+#Generating Training Data
+number = []
+squares = []
+
+for x in range(14):
+  num = x
+  square = num * num
+  number.append(num)
+  squares.append(square)
+
+#Defining Model
+model = Sequential()
+model.add(Dense(1,input_shape = (1,)))
+
+#Compiling Model
+model.compile(loss='mean_squared_error', optimizer=Adam(0.1), metrics=['accuracy'])
+
+#Training Model
+history = model.fit(number, squares, epochs=700, verbose=0)
+
+
+# save model and architecture to single file
+model.save("square-model.h5")
+print("Model saved !")
