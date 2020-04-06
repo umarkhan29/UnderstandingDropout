ebbaaa700a5b74cbd66bbc35578aecd959b6e586
Umar Khan <umee909@gmail.com>
2020-04-06 16:31:01 +0530

    Add files via upload

---
+++ Square/load-square-model.py
@@ -0,0 +1,13 @@
+from keras.models import load_model
+ 
+# load model
+model = load_model('square-model.h5')
+
+
+#enter number to predict
+print("Enter Number: ")
+num = input()
+num=[num]
+
+#Predicting a sample
+print(model.predict([num]))
